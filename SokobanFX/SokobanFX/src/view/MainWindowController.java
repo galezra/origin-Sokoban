1635cc10647448b2a6416a280036696f6dff30f9
sahar <saharmi204@gmail.com>
2017-01-23 08:21:37 +0200

    timer setiing fix

--- SokobanFX/SokobanFX/src/view/MainWindowController.java
+++ SokobanFX/SokobanFX/src/view/MainWindowController.java
@@ -6,14 +6,19 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Observable;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import Controler.SokobanController;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -22,76 +27,39 @@ public class MainWindowController extends Observable  implements Initializable,V
 	@FXML	private SokobanDisplayer sd=new SokobanDisplayer();
 	private String userCommand;
 	private char[][]arr;
+	int count;
+	@FXML
+	Text myText;
+	StringProperty Counter;
 	//private SokobanController SC;
 
 
 
-	public void exit()
-	{
-		this.setUserCommand("exit game");//without saving the game
-	}
-	public void saveFileMethod()
-	{
-		if (arr!=null)
-		{
-			FileChooser fc=new FileChooser();
-			fc.setInitialDirectory(new File("./resources/Levels"));
-			fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text doc(*.txt)", "*.txt"));
-			fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML doc(*.xml)", "*.xml"));
-			fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Object file(*.obj)", "*.obj"));
+	public MainWindowController(){
 	
-			File chosen=fc.showSaveDialog(null);
-			if (chosen!=null)
-			{
-				sd.requestFocus();
-				String fileName=chosen.getName();
-				this.setUserCommand("save "+fileName);
-			}
-		}
+	
+	}
+
+
+	public void startCounter()
+	{
+	
 		
-
+		Timer t=new  Timer();
+		t.scheduleAtFixedRate(new TimerTask() {
+					
+					@Override
+					public void run() {
+						Counter.set(""+(++count));
+					}
+				}, 0, 1000);
 	}
-	public void loadFileMethod()
-	{
-
-	 FileChooser fc = new FileChooser();
-
-	 fc.setTitle("open sokoban level file:");
-	 fc.setInitialDirectory(new File("./resources/Levels"));
-	 File chosen  = fc.showOpenDialog(null);
-	 if (chosen!=null)
-	 {
-		String fileName= chosen.getName();
-		this.setUserCommand("load "+fileName);
-
-
-	 }
-
-	}
-	public SokobanDisplayer getSd() {
-		return sd;
-	}
-
-	public void setSd(SokobanDisplayer sd) {
-		this.sd = sd;
-	}
-
-	public char[][] getArr() {
-		if (arr!=null)
-			return arr;
-		return null;
-	}
-
-	public void setArr(char[][] arr) {
-		this.arr = arr;
-		this.sd.setLevelData(arr);
-	}
-	
-
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-	
+		Counter=new SimpleStringProperty();
+		count=0;
+		myText.textProperty().bind(Counter);
 		sd.addEventFilter(MouseEvent.MOUSE_CLICKED, (e)->sd.requestFocus());
 
 		sd.setOnKeyPressed(new EventHandler<KeyEvent>() {
@@ -140,6 +108,68 @@ public class MainWindowController extends Observable  implements Initializable,V
 		this.setChanged();
 		this.notifyObservers(userCommand);
 	}
+	public void exit()
+	{
+		this.setUserCommand("exit game");//without saving the game
+	}
+	public void saveFileMethod()
+	{
+		if (arr!=null)
+		{
+			FileChooser fc=new FileChooser();
+			fc.setInitialDirectory(new File("./resources/Levels"));
+			fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text doc(*.txt)", "*.txt"));
+			fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML doc(*.xml)", "*.xml"));
+			fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Object file(*.obj)", "*.obj"));
+	
+			File chosen=fc.showSaveDialog(null);
+			if (chosen!=null)
+			{
+				sd.requestFocus();
+				String fileName=chosen.getName();
+				this.setUserCommand("save "+fileName);
+			}
+		}
+		
+
+	}
+	public void loadFileMethod()
+	{
+
+	 FileChooser fc = new FileChooser();
+
+	 fc.setTitle("open sokoban level file:");
+	 fc.setInitialDirectory(new File("./resources/Levels"));
+	 File chosen  = fc.showOpenDialog(null);
+	 if (chosen!=null)
+	 {
+		String fileName= chosen.getName();
+		this.setUserCommand("load "+fileName);
+
+
+	 }
+	 this.startCounter();
+
+	}
+	public SokobanDisplayer getSd() {
+		return sd;
+	}
+
+	public void setSd(SokobanDisplayer sd) {
+		this.sd = sd;
+	}
+
+	public char[][] getArr() {
+		if (arr!=null)
+			return arr;
+		return null;
+	}
+
+	public void setArr(char[][] arr) {
+		this.arr = arr;
+		this.sd.setLevelData(arr);
+	}
+	
 
 
 
@@ -147,3 +177,4 @@ public class MainWindowController extends Observable  implements Initializable,V
 
 
 }
+
