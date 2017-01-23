45cfe66da468b7916fba359afc828037e0451629
sahar <saharmi204@gmail.com>
2017-01-23 12:40:32 +0200

    23/1/17 12:40

--- SokobanFX/SokobanFX/src/view/Main.java
+++ SokobanFX/SokobanFX/src/view/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 			SokobanController sc=new SokobanController();
 			mwc.addObserver(sc);
 			sc.setMWC(mwc);
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,650,650);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
